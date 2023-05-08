2103d093eb782624c9d2512f83929c3d754f39fc
K-b <bridgefield738@gmail.com>
2023-05-08 14:39:21 +0900

    [Add] frontend component file [update] App.tsx Join Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import RouterCompornent from './compornents/router';
 
-function App() {
+
+const App: React.FC = () => {
+  
+  const items = Object.values(1)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-8">
+              <div className="card">
+                <div>
+                        
+                  <RouterCompornent setStateProp={items} />
+                  {/* <DrawTable setStateProp ={items2} /> */}
+                </div>
+            </div>
+          </div>
+      </div>
     </div>
-  );
+  )
 }
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
 export default App;
+
+try{
+  if (document.getElementById('App')) {
+      const Index = ReactDOM.createRoot(document.getElementById("App"));
+      const element = document.getElementById('example')
+      //const props = JSON.parse(element.dataset.props)
+
+      Index.render(
+          <React.StrictMode>
+              <App {...props}/>
+          </React.StrictMode>
+      )
+}}catch(e){
+      console.log(e)
+}
---
+++ src/compornents/router.tsx
@@ -0,0 +1,38 @@
+import React, {FC, Dispatch, SetStateAction} from 'react';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+
+interface setStateProps{
+
+    setStateProp: object;
+}
+
+
+export default function RouterCompornent(props: setStateProps) {
+    
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div>
+                            <BrowserRouter>
+                                <Routes>
+                                    <Route element={<ResponsiveDrawer/>}>
+                                        <Route path="/welcome"  element={<Table setStateProp={items} />} />
+                                        <Route path="/element" element={<EnhancedTable />} />
+                                        <Route path="/email" element={<SortTable/>} />
+                                        <Route path="/dashboard" element={<Dashboard/>} />
+                                        <Route path="/settings" element={<SettingPCS/>} />
+                                    </Route>
+                                </Routes>
+                            </BrowserRouter>
+                            {/* <DrawTable setStateProp ={items2} /> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        );
+}
