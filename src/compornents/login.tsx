b1a5953bf55379b020b0b09d056033cf1d8af3de
K-b <bridgefield738@gmail.com>
2023-05-15 13:10:11 +0900

    [update]パスワードのハッシュ化処理

--- src/compornents/login.tsx
+++ src/compornents/login.tsx
@@ -11,6 +11,7 @@ import { memo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { User } from "./types";
 import { loginUser } from './authContext';
+import CryptoJS from 'crypto-js';
 
 export const Login = memo(() => {
   const [username, setUsername] = useState<number | null>(null);
@@ -26,9 +27,21 @@ export const Login = memo(() => {
     variant: "outlined",
   };
 
+  const SALT = 'your-unique-salt-here';
+
+  const hashPassword = (password: string): string => {
+    const hashedPassword = CryptoJS.PBKDF2(password, SALT, {
+      keySize: 256 / 32,
+      iterations: 1000,
+    }).toString(CryptoJS.enc.Hex);
+
+    return hashedPassword;
+  }
+
   const onClickLogin = async () => {
     if(username !== null) {
-      const user = await loginUser(username, password);
+      const hashedPassword = hashPassword(password);
+      const user = await loginUser(username, hashedPassword);
       if (user) {
         navigate("/home");
       } else {
