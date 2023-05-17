00d7e158d6b7cec9954192d2f3ccf70288c0dce5
K-b <bridgefield738@gmail.com>
2023-05-17 15:44:11 +0900

    [update]予約ページの仮組

--- src/compornents/userReserve.tsx
+++ src/compornents/userReserve.tsx
@@ -1,7 +1,41 @@
 
-function ReservationPage() {
-    return <div>ReservationPage</div>;
+
+import React, { useState } from 'react';
+import { Button, Typography } from '@mui/material';
+
+interface ReservationPageProps {
+  date: string;
+  isReserved: boolean;
 }
 
+const ReservationPage: React.FC<ReservationPageProps> = ({ date, isReserved }) => {
+  const [reserved, setReserved] = useState(isReserved);
 
-export default ReservationPage; 
+  const handleReservation = () => {
+    setReserved(true);
+    // TODO: Add reservation logic here
+  };
+
+  const handleCancelReservation = () => {
+    setReserved(false);
+    // TODO: Add cancel reservation logic here
+  };
+
+  return (
+    <div>
+      <Typography variant="h4">Reservation for {date}</Typography>
+      <Typography variant="h6">{reserved ? 'Reserved' : 'Not reserved'}</Typography>
+      {reserved ? (
+        <Button variant="contained" color="error" onClick={handleCancelReservation}>
+          Cancel Reservation
+        </Button>
+      ) : (
+        <Button variant="contained" color="primary" onClick={handleReservation}>
+          Reserve
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default ReservationPage;
