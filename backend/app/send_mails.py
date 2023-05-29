834710b6b6a08709cf2d39f7ae8b6a6e0e4b2b61
K-b <bridgefield738@gmail.com>
2023-05-29 13:04:08 +0900

    [update] メール送信関数の実装

---
+++ backend/app/send_mails.py
@@ -0,0 +1,32 @@
+"""
+予約情報をもとに、予約したユーザーにメールを送信する
+"""
+from datetime import datetime, timedelta, timezone
+from backend.app.models import Reservation, User
+from backend.app import db
+from backend.app.functions import generate_token
+from backend.app import app
+from flask_mail import Message
+
+# メール送信用の関数
+def send_mail():
+    # 現在の日時を取得
+    now = datetime.now(timezone.utc)
+
+    # 当日の予約情報を取得
+    reservations = Reservation.query.filter(Reservation.date == now.date()).all()
+
+    # 予約情報がある場合にメールを送信
+    if reservations:
+        for reservation in reservations:
+            # 予約したユーザーを取得
+            user = User.query.get(reservation.user_id)
+
+            # メールの内容を作成
+            msg = Message('予約のお知らせ', recipients=[user.email])
+            msg.body = f'{user.name}さん、{reservation.date}の予約があります。'
+            msg.html = f'<p>{user.name}さん、{reservation.date}の予約があります。</p>'
+
+            # メールを送信
+            with app.app_context():
+                mail.send(msg)
